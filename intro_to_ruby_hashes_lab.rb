f0bcb9754af879ffb30ac965dab1ae4a7da91901
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-21 21:13:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-   hash.fetch(key)  { |key| 1 }
-
+  #  hash.fetch(key)  { |key| 1 }
+   hash[key] || 1
   # count=0
   #  if hash[key]
   #     hash [key]  += 1
